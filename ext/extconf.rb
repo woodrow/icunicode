ce6f5df5164d63cac0a07ced6a6674dc11c6e205
Andreas Fuchs <asf@boinkor.net>
2013-10-17 11:37:58 -0700

    Find the unicode headers in the right places

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,8 @@
 require 'mkmf'
 have_library('icui18n')
 have_library('icuio')
+find_header('unicode/ucol.h',
+            '/usr/local/include',
+            '/opt/local/include',
+            '/usr/local/opt/icu4c/include')
 create_makefile('icunicode')
