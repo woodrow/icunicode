d2fedadf278834b4a9548f2ea67b40c381dc427d
Andreas Fuchs <asf@boinkor.net>
2013-10-17 11:39:16 -0700

    Attempt to find the unicode headers in /usr/include also

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,6 +2,7 @@ require 'mkmf'
 have_library('icui18n')
 have_library('icuio')
 find_header('unicode/ucol.h',
+            '/usr/include',
             '/usr/local/include',
             '/opt/local/include',
             '/usr/local/opt/icu4c/include')
