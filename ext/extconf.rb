be528254e587c7b2ab936dae5a9164445027882d
Andreas Fuchs <asf@boinkor.net>
2013-10-25 14:56:44 -0700

    Find libraries in the usual paths, also
    This should fix any build issues on OS X 10.9.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,9 +1,14 @@
 require 'mkmf'
-have_library('icui18n')
-have_library('icuio')
+
 find_header('unicode/ucol.h',
             '/usr/include',
             '/usr/local/include',
             '/opt/local/include',
             '/usr/local/opt/icu4c/include')
+
+LIB_DIRS=['/usr/local/opt/icu4c/lib', '/usr/lib', '/usr/local/lib', '/opt/local/lib']
+
+find_library('icui18n', nil, *LIB_DIRS)
+find_library('icuio', nil, *LIB_DIRS)
+
 create_makefile('icunicode')
